b6e41e04f17ca282d9ce65c2934fa37b10d7f690
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-10-04 20:33:44 +0200

    menu drop animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
-const btnOpenMenu = document.getElementById('navigation_burger-btn');
-const btnCloseMenu = document.querySelector('#container-close-button');
+const btnToggleMenu = document.getElementById('navigation_burger-btn');
 const asideMenu = document.getElementById('navigation_aside-menu');
 const menu = document.getElementById('navigation_menu');
 
-btnOpenMenu.addEventListener('click', toggleMenu)
+btnToggleMenu.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
     menu.classList.toggle('active')
